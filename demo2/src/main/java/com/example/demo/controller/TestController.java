cde97324399e6b475e6917431597a1b5c30d2942
xiayin <1092931091@qq.com>
2019-04-16 08:55:24 +0800

    demo

---
+++ demo2/src/main/java/com/example/demo/controller/TestController.java
@@ -0,0 +1,18 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * @author arthas on 2019/4/15
+ */
+@RestController
+@RequestMapping("/")
+public class TestController {
+
+    @RequestMapping(value = "/test", method = RequestMethod.GET)
+    public String test() {
+        return "success";
+    }
+}
